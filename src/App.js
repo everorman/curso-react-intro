6964b521e92de2e8960f6abf7759fd1ea870bdd8
Evert Ortiz <evert.ortiz.m@gmail.com>
2024-05-31 10:31:34 -0500

    refactor: adding states and events

--- src/App.js
+++ src/App.js
@@ -5,20 +5,26 @@ import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { TodoSearch } from './TodoSearch';
 import { CreateTodo } from './CreateTodo';
-
+import React from 'react';
 const defaultTODOS = [
   { text: "Todo 1", completed: true },
   { text: "Todo 2", completed: false },
   { text: "Todo 3", completed: false },
   { text: "Todo 4", completed: false },
+  { text: "Todo 5", completed: true },
 ];
 
 function App() {
+  const [searchTodo, setSearchTodo] = React.useState('');
+  const [todos, setTodos] = React.useState(defaultTODOS);
+  console.log('value', searchTodo);
+  const completedTodos = todos.filter(item => !!item.completed).length;
+  const totalTodos = todos.length;
   return (
     <>
-      <TodoCounter completed={1} total={5} />
+      <TodoCounter completed={completedTodos} total={totalTodos} />
       <div className="m-2">
-        <TodoSearch />
+        <TodoSearch searchTodo={searchTodo} setSearchTodo={setSearchTodo} />
         <TodoList>
           {defaultTODOS.map(todo => (<TodoItem key={todo.text} text={todo.text} completed={todo.completed} />))}
         </TodoList>
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,10 +1,17 @@
+import React from 'react';
+export const TodoSearch = ({ searchTodo, setSearchTodo }) => {
 
-export const TodoSearch = () => {
+  const searchEvent = (event) => {
+    const { value } = event.target;
+    setSearchTodo(value);
+  };
   return (
     <input
+      value={searchTodo}
       type="text"
       placeholder="Buscar TODO"
       className="w-full p-2 border rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      onChange={searchEvent}
     />
   );
 };
