ce7f49f15cf529ce3f92338f32e0271c74b280f1
Evert Ortiz <evert.ortiz.m@gmail.com>
2024-05-31 11:16:43 -0500

    chore: deleting todos

--- src/App.js
+++ src/App.js
@@ -20,13 +20,39 @@ function App() {
   console.log('value', searchTodo);
   const completedTodos = todos.filter(item => !!item.completed).length;
   const totalTodos = todos.length;
+  const searchedTodos = todos.filter((item) => item.text.toLowerCase().includes(searchTodo.toLowerCase()));
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const index = newTodos.findIndex(item => item.text === text);
+    newTodos[index].completed = true;
+    setTodos(newTodos);
+  };
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const index = newTodos.findIndex(item => item.text === text);
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <>
       <TodoCounter completed={completedTodos} total={totalTodos} />
       <div className="m-2">
-        <TodoSearch searchTodo={searchTodo} setSearchTodo={setSearchTodo} />
+        <TodoSearch
+          searchTodo={searchTodo}
+          setSearchTodo={setSearchTodo}
+        />
         <TodoList>
-          {defaultTODOS.map(todo => (<TodoItem key={todo.text} text={todo.text} completed={todo.completed} />))}
+          {searchedTodos.map(todo => (
+            <TodoItem
+              key={todo.text}
+              text={todo.text}
+              completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
+            />
+          ))}
         </TodoList>
         <CreateTodo />
       </div>
