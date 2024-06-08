82746d96025ce0eb6edaf50918945ecc95006e5c
Evert Ortiz <evert.ortiz.m@gmail.com>
2024-06-08 14:04:08 -0500

    chore: creating custom hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './platzi.webp';
 import './App.css';
 import { TodoCounter } from './TodoCounter';
 import { TodoList } from './TodoList';
@@ -6,35 +5,41 @@ import { TodoItem } from './TodoItem';
 import { TodoSearch } from './TodoSearch';
 import { CreateTodo } from './CreateTodo';
 import React from 'react';
-const defaultTODOS = [
-  { text: "Todo 1", completed: true },
-  { text: "Todo 2", completed: false },
-  { text: "Todo 3", completed: false },
-  { text: "Todo 4", completed: false },
-  { text: "Todo 5", completed: true },
-];
+// const defaultTODOS = [
+//   { text: "Todo 1", completed: true },
+//   { text: "Todo 2", completed: false },
+//   { text: "Todo 3", completed: false },
+//   { text: "Todo 4", completed: false },
+//   { text: "Todo 5", completed: true },
+// ];
+
+const useLocalStorage = (itemKey, initialValue) => {
+  const localStorageItem = localStorage.getItem(itemKey);
+  let parseTodos = initialValue;
+  if (!localStorageItem) {
+    localStorage.setItem(itemKey, JSON.stringify(initialValue));
+  } else {
+    parseTodos = JSON.parse(localStorage.getItem(itemKey));
+  }
+
+  const [item, setItem] = React.useState(parseTodos);
+
+  const saveItem = (newItem) => {
+    localStorage.setItem(itemKey, JSON.stringify(newItem));
+    setItem(newItem);
+
+  };
+  return [item, saveItem];
+};
 
 function App() {
-  const TODOS_STORAGE_KEY = 'TODOS_V1';
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
   const [searchTodo, setSearchTodo] = React.useState('');
-  const localStorageTODOS = localStorage.getItem(TODOS_STORAGE_KEY);
-  let parseTodos = [];
-  if (!localStorageTODOS) {
-    localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify([]));
-  } else {
-    parseTodos = JSON.parse(localStorage.getItem(TODOS_STORAGE_KEY));
-  }
-  const [todos, setTodos] = React.useState(parseTodos);
-  console.log('value', searchTodo);
   const completedTodos = todos.filter(item => !!item.completed).length;
   const totalTodos = todos.length;
   const searchedTodos = todos.filter((item) => item.text.toLowerCase().includes(searchTodo.toLowerCase()));
 
-  const saveTodos = (newTodos) => {
-    localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(newTodos));
-    setTodos(newTodos);
 
-  };
   const completeTodo = (text) => {
     const newTodos = [...todos];
     const index = newTodos.findIndex(item => item.text === text);
